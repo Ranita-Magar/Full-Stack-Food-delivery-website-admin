819493b8dbeb3eeacc62550e1629c3d167c48978
Ranita-Magar <87315393+Ranita-Magar@users.noreply.github.com>
2024-04-22 20:59:57 +0545

    Displaying Orders In Admin Panel

---
+++ src/pages/Order/Order.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import "./Order.css";
+import { useState } from "react";
+import toast from "react-toastify";
+import { useEffect } from "react";
+import axios from "axios";
+import { assets } from "../../assets/assets";
+
+export const Order = ({ url }) => {
+  const [orders, setOrders] = useState([]);
+
+  const fetchAllOrders = async () => {
+    const response = await axios.get(url + "/api/order/list");
+    if (response.data.success) {
+      setOrders(response.data.data);
+      console.log(response.data.data);
+    } else {
+      toast.error("Error");
+    }
+  };
+
+  useEffect(() => {
+    fetchAllOrders();
+  }, []);
+
+  return (
+    <div className="order-add">
+      <h3>Order Page</h3>
+      <div className="order-list">
+        {orders.map(() => {
+          <div key={index} className="order-item">
+            <img src={assets.parcel_icon} alt="" />
+            <div>
+              <p className="order-item-food">
+                {orders.items.map((item, index) => {
+                  if (index === orders.items.length - 1) {
+                    return item.name + "x" + item.quantity;
+                  } else {
+                    return item.name + "x" + item.quantity + ",";
+                  }
+                })}
+              </p>
+              <p className="order-item">
+                {orders.address.firstName + " " + orders.address.lastName}{" "}
+              </p>
+              <div className="order-item-address">
+                <p>{orders.address.streets + ","} </p>
+                <p>
+                  {orders.address.city +
+                    ", " +
+                    orders.address.state +
+                    ", " +
+                    orders.address.country +
+                    ", " +
+                    orders.address.zipcode}
+                </p>
+              </div>
+              <p className="order-item-phone">{orders.address.phone}</p>
+            </div>
+            <p>Items: {orders.items.length} </p>
+            <p>${orders.amount}</p>
+            <select>
+              <option value="Food Processing">Food Processing</option>
+              <option value="Out for Delivery">Out for Delivery</option>
+              <option value="Delivered">Delivered</option>
+            </select>
+          </div>;
+        })}
+      </div>
+    </div>
+  );
+};
